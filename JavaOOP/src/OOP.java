73c4a1a9e9fcb013b1cb05018130564b446bf13d
ahmad1542 <ahmad.husien4@gmail.com>
2024-07-07 00:00:27 +0300

    asd

--- JavaOOP/src/OOP.java
+++
@@ -1,7 +0,0 @@
-public class OOP {
-    public static void main(String[] args) {
-        Animal an = new Animal("Albatross",3, "Growling");
-        Birds bd = new Birds("Albatross",4, "Growling","Gliding");
-
-    }
-}
